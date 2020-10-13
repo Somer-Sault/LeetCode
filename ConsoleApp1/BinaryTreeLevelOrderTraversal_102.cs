d6d83d73390381d4475ca741ea0a70fc78ec7f67
somersault <65251927+Somer-Sault@users.noreply.github.com>
2020-10-13 21:47:08 +0300

    Improved in terms of space complexity O(1) - no additional classes and dictionary

--- ConsoleApp1/BinaryTreeLevelOrderTraversal_102.cs
+++ ConsoleApp1/BinaryTreeLevelOrderTraversal_102.cs
@@ -7,38 +7,61 @@ namespace ConsoleApp1
   {
     public IList<IList<int>> LevelOrder(TreeNode root)
     {
-      // check inputs
-      if (root == null)
-        return new List<IList<int>>();
-
-      // Space: O(N)
+      // Variant 1
+      // Space: O(1)
       // Runtime: O(N)
-
-      var map = new Dictionary<int/*lvl*/, IList<int> /*values*/>();
-      var queue = new Queue<TreeNodeEx>();
-      queue.Enqueue(new TreeNodeEx(root, 0));
-      int level = 0;
-      // [3,9,20,null,null,15,7]
-      // q: 3,17 3,15
-      // m: (0, {3}) (1, {9,20}) 3, {15,7 }
-      // l: 3
+      var result = new List<IList<int>>();
+      if (root == null)
+        return result;
+      var queue = new Queue<TreeNode>();
+      queue.Enqueue(root);
       while (queue.Count > 0)
       {
-        TreeNodeEx node = queue.Dequeue();
-        if (node == null) continue;
+        //Now, we're going via all nodes of a single level of size levelSize
+        int levelSize = queue.Count;
+        var levelList = new List<int>(levelSize);
+        for (int i = 0; i < levelSize; i++)
+        {
+          var node = queue.Dequeue();
+          if (node == null) continue;
+          levelList.Add(node.val);
 
-        if (!map.ContainsKey(node.lvl))
-          map[node.lvl] = new List<int> { node.val };
-        else
-          map[node.lvl].Add(node.val);
-
-        if (node.left != null)
-          queue.Enqueue(new TreeNodeEx(node.left, node.lvl + 1));
-        if (node.right != null)
-          queue.Enqueue(new TreeNodeEx(node.right, node.lvl + 1));
+          if (node.left != null)
+            queue.Enqueue(node.left);
+          if (node.right != null)
+            queue.Enqueue(node.right);
+        }
+        result.Add(levelList);
       }
 
-      return map.Values.ToList();
+      return result;
+
+      // Variant 2
+      // Space: O(N)
+      // Runtime: O(N)
+      //if (root == null)
+      //  return new List<IList<int>>();
+      //var map = new Dictionary<int/*lvl*/, IList<int> /*values*/>();
+      //var queue = new Queue<TreeNodeEx>();
+      //queue.Enqueue(new TreeNodeEx(root, 0));
+      //int level = 0;
+      //while (queue.Count > 0)
+      //{
+      //  TreeNodeEx node = queue.Dequeue();
+      //  if (node == null) continue;
+
+      //  if (!map.ContainsKey(node.lvl))
+      //    map[node.lvl] = new List<int> { node.val };
+      //  else
+      //    map[node.lvl].Add(node.val);
+
+      //  if (node.left != null)
+      //    queue.Enqueue(new TreeNodeEx(node.left, node.lvl + 1));
+      //  if (node.right != null)
+      //    queue.Enqueue(new TreeNodeEx(node.right, node.lvl + 1));
+      //}
+
+      //return map.Values.ToList();
     }
   }
 
